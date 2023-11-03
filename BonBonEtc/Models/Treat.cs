2a0da6c32a235eec78f290189d234293fb6e0563
WillJolley <yeswilljolley@gmail.com>
2023-11-02 22:39:41 -0700

    set up models

---
+++ BonBonEtc/Models/Treat.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace BonBonEtc.Models
+{
+  public class Treat
+  {
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public List<FlavorTreat> FlavorTreats { get; set; }
+  }
+}
